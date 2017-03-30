723b9e7354a90fb218097197803d9b1feea4f15b
Luis Chan <luis.chan@qq.com>
2017-03-30 17:09:36 +0800

    test travis

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
+	c.Data["Website"] = "www.baidu1.com"
 	c.Data["Email"] = "luis.chan@qq.com"
 	c.TplName = "index.tpl"
 }
